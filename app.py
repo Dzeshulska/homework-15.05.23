0121c8fee833c79409ece148d9d45a783a63e6db
Alla_Dseshulska <dzeshulska@gmail.com>
2023-05-10 22:10:47 +0300

    added element search

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
 from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.common.keys import Keys
+
+import time
+
+#set chromodriver.exe path
+
+driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")
+
+driver.implicitly_wait(0.5)
+
+#launch URL(поиск элемента на странице по XPATH)
+
+driver.get("https://medium.com/")
+time.sleep(5)
+elem = driver.find_elements(By.XPATH,  '/html/body/div[1]/div/div[1]/nav/div/a[1]')
+print(elem)
+time.sleep(10)
+
+
+
+
+
+
